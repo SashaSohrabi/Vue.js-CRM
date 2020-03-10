c4b5d99020cf91d7b7b84990b6837444a03f6067
sashasohrabi <shb.sohrabi@gmail.com>
2020-03-11 00:46:33 +0200

    realized date and time, added date.filter

---
+++ src/filters/date.filter.js
@@ -0,0 +1,19 @@
+const dataFilter = (date, format = "date") => {
+  const options = {};
+
+  if (format.includes("date")) {
+    options.day = "2-digit";
+    options.month = "long";
+    options.year = "numeric";
+  }
+
+  if (format.includes("time")) {
+    options.hour = "2-digit";
+    options.minute = "2-digit";
+    options.second = "2-digit";
+  }
+
+  return new Intl.DateTimeFormat("ru-RU", options).format(date);
+};
+
+export default dataFilter;
