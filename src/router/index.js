b708968bb551f1304acb444477f51ed9fed4314c
sashasohrabi <shb.sohrabi@gmail.com>
2020-04-18 21:58:58 +0300

    added navigation guards to routers, update to MainLayout

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import firebase from "firebase/app";
 
 Vue.use(VueRouter);
 
@@ -8,62 +9,73 @@ const routes = [
     path: "/login",
     name: "login",
     meta: { layout: "auth" },
-    component: () => import("../views/Login.vue")
+    component: () => import("../views/Login.vue"),
   },
   {
     path: "/register",
     name: "register",
     meta: { layout: "auth" },
-    component: () => import("../views/Register.vue")
+    component: () => import("../views/Register.vue"),
   },
   {
     path: "/",
     name: "home",
-    meta: { layout: "main" },
-    component: () => import("../views/Home.vue")
+    meta: { layout: "main", auth: true },
+    component: () => import("../views/Home.vue"),
   },
   {
     path: "/categories",
     name: "categories",
-    meta: { layout: "main" },
-    component: () => import("../views/Categories.vue")
+    meta: { layout: "main", auth: true },
+    component: () => import("../views/Categories.vue"),
   },
   {
     path: "/detail/:id",
     name: "detail",
-    meta: { layout: "main" },
-    component: () => import("../views/Detail.vue")
+    meta: { layout: "main", auth: true },
+    component: () => import("../views/Detail.vue"),
   },
   {
     path: "/history",
     name: "history",
-    meta: { layout: "main" },
-    component: () => import("../views/History.vue")
+    meta: { layout: "main", auth: true },
+    component: () => import("../views/History.vue"),
   },
   {
     path: "/planning",
     name: "planning",
-    meta: { layout: "main" },
-    component: () => import("../views/Planning.vue")
+    meta: { layout: "main", auth: true },
+    component: () => import("../views/Planning.vue"),
   },
   {
     path: "/profile",
     name: "profile",
-    meta: { layout: "main" },
-    component: () => import("../views/Profile.vue")
+    meta: { layout: "main", auth: true },
+    component: () => import("../views/Profile.vue"),
   },
   {
     path: "/record",
     name: "record",
-    meta: { layout: "main" },
-    component: () => import("../views/Record.vue")
-  }
+    meta: { layout: "main", auth: true },
+    component: () => import("../views/Record.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiredAuth = to.matched.some((record) => record.meta.auth);
+
+  if (requiredAuth && !currentUser) {
+    next("/login?message=login");
+  } else {
+    next();
+  }
 });
 
 export default router;
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,9 +1,9 @@
 const messages = {
   logout: "Вы вышли из системы",
+  login: "Для начала войдите в систему",
   "auth/user-not-found": "Пользователя с таким email не существует",
   "auth/wrong-password": "Неверный пароль",
-  "auth/email-already-in-use": "Email уже занят"
-
+  "auth/email-already-in-use": "Email уже занят",
 };
 
 export default messages;
