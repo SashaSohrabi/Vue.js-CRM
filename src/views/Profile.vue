2514dd5694857204474be7a1d8434c15feeaa397
sashasohrabi <shb.sohrabi@gmail.com>
2020-04-28 12:42:44 +0300

    added switch button to Profile page, retrieved user's name from store and updated name field in Profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,9 +6,18 @@
 
     <form class="form">
       <div class="input-field">
-        <input id="description" type="text" />
+        <input id="description" type="text" v-model="name" />
         <label for="description">Имя</label>
         <span class="helper-text invalid">name</span>
+      </div>
+
+      <div class="switch">
+        <label>
+          Off
+          <input type="checkbox" />
+          <span class="lever"></span>
+          On
+        </label>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -18,3 +27,27 @@
     </form>
   </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  data: () => ({
+    name: ""
+  }),
+  mounted() {
+    this.name = this.info.name;
+    setTimeout(() => {
+      M.updateTextFields();
+    });
+  },
+  computed: {
+    ...mapGetters(["info"])
+  }
+};
+</script>
+
+<style scoped>
+.switch {
+  margin-bottom: 2rem;
+}
+</style>
