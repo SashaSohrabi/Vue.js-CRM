b551a898190cbc044bd50cca99b78a56db35138e
sashasohrabi <shb.sohrabi@gmail.com>
2020-04-29 00:17:43 +0300

    removed commented out code

--- src/utils/message.plugin.js
+++ src/utils/message.plugin.js
@@ -1,18 +1,4 @@
-// const messagePlugin = {
-//   install(Vue, options) {
-//     Vue.prototype.$message = function(text) {
-//       window.M.toast({ html: text });
-//     };
-
-//     Vue.prototype.$error = function(text) {
-//       window.M.toast({ htm: `[ошибка]: ${text}` });
-//     };
-
-//     console.log(options);
-//   }
-// };
-
-const messagePlugin = Vue => {
+const messagePlugin = (Vue) => {
   Vue.prototype.$message = function(text) {
     window.M.toast({ html: text });
   };
